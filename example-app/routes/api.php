a8d61be91e154fe79ff0076beef949f68a67c941
Kamil-Klecz <kk305373@student.polsl.pl>
2022-12-18 22:31:11 +0100

    Update Route api.php
    Replaced the individual method routes with a single universal route.

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\requests;
+use App\Http\Controllers\PeopleController;
 
 
-Route::get('people', [requests::class, 'index']);
-Route::get('people/{id}', [requests::class, 'show']);
-Route::post('people', [requests::class, 'create']);
-Route::get('people/{id}', [requests::class, 'update']);
-Route::get('people/{id}', [requests::class, 'destroy']);
-
-Route::apiResource('peoples', requests::class);
+Route::apiResource('peoples', PeopleController::class);
