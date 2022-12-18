984b9106f1c1fe4a2e3153785f04b118bcfc63f5
Kamil-Klecz <kk305373@student.polsl.pl>
2022-12-18 19:21:37 +0100

    Update Controller requests.php
    Updated the requests.php controller with remaining method functions

--- example-app/app/Http/Controllers/requests.php
+++ example-app/app/Http/Controllers/requests.php
@@ -3,64 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\People;
+use Illuminate\Http\JsonResponse;
+use App\Requests\PeoplePostRequest;
+use Illuminate\Support\Facades\DB;
 
-class requests extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+class PeopleController extends Controller{
+
+    public function index(): JsonResponse
     {
-        //
-        
-
+        return response()->json(People::all(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function create(Request $request): JsonResponse
+    {  
+            $people = new People;
+            $people->category_name = $request->category_name;
+            $people->save();
+            return response()->json(null, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show($id): JsonResponse
     {
-        //
+       $people = DB::table('people')->where('ID', $id)->first();
+       return response()->json($people);
     }
 
-   
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, People $people): JsonResponse
     {
-        //
+        $people->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => "People Updated successfully!",
+            'people' => $people
+        ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy($id): JsonResponse
     {
-        //
+        DB::table('people')->where('ID', $id)->delete();
+        return response()->json(null, 200);
     }
 }
